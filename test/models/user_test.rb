f906231b29762b9165cc78d2985c24a3ea8e8595
josephcorbin91 <joseph.corbin91@yahoo.com>
2017-10-03 12:53:07 -0700

    6.3

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,4 +14,45 @@ class UserTest < ActiveSupport::TestCase
 		@user.name =  "   "
 		assert_not @user.valid?
 	end
+	
+	test "name should not be too long" do 
+		@user.name = "a" * 51
+		assert_not @user.valid?
+	end
+	
+	test "email should not be too long" do
+		@user.email = "a" * 244 + "@example.com"
+		assert_not @user.valid?
+		end
+		
+	test "email validation should accept valid addresses" do
+		valid_addresses = %w[user@example.com USER@foo.COM A@foo.bar.org firs.la@foo.jp alice+bob@bax.cn]
+		valid_addresses.each do |valid_address|
+			@user.email = valid_address
+			assert @user.valid?, "#{valid_address.inspect} should be valid"
+		end
+	end
+	
+	test "email validation should reject invalid addresses" do
+		invalid_addresses = %w[user@example,com USER_at_foo.COM username@example. foo@bar_bax.com alice@foo+bob.cn]
+		invalid_addresses.each do |invalid_address|
+			@user.email = invalid_address
+			assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+		end
+	end
+	
+	test "email adressses should be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+		end
+		
+	test "email addresses should be saved as lower-case" do
+		mixed_case_email = "Foo@ExaMPle.CoM"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
+	
 end
